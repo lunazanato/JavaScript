20261d0fd933c69c434ec028532610c9ed3f11a4
Luna Zanato <lunanatobraga@gmail.com>
2025-07-04 17:07:20 -0300

    atualizado 04/07

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,13 +1,6 @@
-/* var vel = 12
+var vel = 12
 console.log(`a velocidade de seu carro é ${vel}Km/h`)
 if (vel > 60){ // condição simples 
     console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
 }
-console.log('dirija sempre usando um cinto de segurança') */
-
-var país = 'Brasil'
-if (país == 'Brasil'){
-    console.log('Brasileiro')
-}else{
-    console.log('Estrangeiro')
-}
+console.log('dirija sempre usando um cinto de segurança')
