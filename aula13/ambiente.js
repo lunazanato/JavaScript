11c42a91872cc5f7ac34dc8d1a6bb43672450418
Luna Zanato <lunanatobraga@gmail.com>
2025-07-11 18:19:11 -0300

    atualizado 11/07

---
+++ aula13/ambiente.js
@@ -0,0 +1,26 @@
+/* var c = 1
+while (c <= 6) {
+    console.log('tudo bem?')
+    c++
+}  */
+
+/*  var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}  */
+
+var c = 1    
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+
+
+/* console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?') */
---
+++ aula14/ambiente.js
@@ -0,0 +1,10 @@
+/* var c = 1
+do{
+    console.log(c)
+    c++
+} while (c <=5) */
+console.log('vai começar...')
+for (var c = 1 ;c <= 5 ;c++ ){
+    console.log(c)
+}
+console.log('fim!')
